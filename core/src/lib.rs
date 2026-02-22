66866730c5ec39015d655dae00fcca7fedf558cc
atrtde <trotelalexandre@proton.me>
2026-02-21 20:03:56 -0500

    feat: add parser to core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,7 +1,9 @@
+pub mod parser;
 pub mod priority;
 pub mod tags;
 pub mod types;
 
+pub use parser::DEFAULT_REGEX;
 pub use priority::Priority;
 pub use tags::{DEFAULT_TAGS, TagDefinition};
 pub use types::{FileResult, ScanResult, ScanSummary, TodoItem};
